631491087634e3c7725364d914e4e29554d5c819
Jonathan Pho <jonpho@gmail.com>
2019-06-11 19:50:08 -0500

    Updated Hooks and the Selenium Library

--- src/main/java/utilities/DriverFactory.java
+++ src/main/java/utilities/DriverFactory.java
@@ -6,11 +6,14 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DriverFactory {
 
+//    Make sure your Chrome Version matches your ChromeDriver exe version in the Browsers Folder
 
     public static WebDriver getDriver(String browser) {
-
+        //TODO: Expand this to support Edge and Possibly Safari
         switch (browser) {
             case "chrome":
+                //TODO: Update this and expand to it's own method so it pulls path for both windows path and POSIX
+                System.setProperty("webdriver.chrome.driver", "./src/main/java/utilities/browsers/chromedriver");
                 return new ChromeDriver();
             case "firefox":
                 return new FirefoxDriver();
