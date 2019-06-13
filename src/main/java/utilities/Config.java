252bef214e79ed7964354e15b7602095f3f1cf0d
Jonathan Pho <jonpho@gmail.com>
2019-06-13 00:05:40 -0500

    Updated driverfactory with methods ot set drivers and added comments

--- src/main/java/utilities/Config.java
+++ src/main/java/utilities/Config.java
@@ -15,6 +15,11 @@ public class Config {
 
     public static String BASE_URL;
 
+    /**
+     * This method loads the property file from the resources folder to set basic environment variables depending on
+     * what is needed.
+     * @param env The ENV string is actually set in the constant variable on this page. Or it can bet set in CMD.
+     */
     public void loadProperties(String env) {
         try {
             Properties properties = new Properties();
@@ -30,7 +35,13 @@ public class Config {
         }
     }
 
-    public String getPropertiesFile(String env) {
+    /**
+     * This method returns the string file name of the properties file in the resources folder. This lets us have more
+     * then one property file to load.
+     * @param env The ENV setting is set by the constant variable on in this file at the top of the page.
+     * @return This returns the property file name from the resources folder.
+     */
+    private String getPropertiesFile(String env) {
         switch(env.toUpperCase()) {
             case "TEST":
                 return "test.properties";
--- src/main/java/utilities/DriverFactory.java
+++ src/main/java/utilities/DriverFactory.java
@@ -8,17 +8,56 @@ public class DriverFactory {
 
 //    Make sure your Chrome Version matches your ChromeDriver exe version in the Browsers Folder
 
+    /**
+     * This method set the driver property and sets the path where the binary is located. Then sends the driver type to before Hook.
+     * @param browser The browser type is currently set in the config file constant variable BROWSER.
+     * @return Returns driver type back to the before hook for setup. Currently it will be either chrome or firefox.
+     */
     public static WebDriver getDriver(String browser) {
-        //TODO: Expand this to support Edge and Possibly Safari
+        /* TODO: Expand this to support Edge */
         switch (browser) {
             case "chrome":
-                //TODO: Update this and expand to it's own method so it pulls path for both windows path and POSIX
-                System.setProperty("webdriver.chrome.driver", "./src/main/java/utilities/browsers/chromedriver");
+                System.setProperty("webdriver.chrome.driver", getBrowserPath(browser));
                 return new ChromeDriver();
             case "firefox":
-                System.setProperty("webdriver.gecko.driver", "./src/main/java/utilities/browsers/geckodriver");
+                System.setProperty("webdriver.gecko.driver", getBrowserPath(browser));
                 return new FirefoxDriver();
         }
         return null;
+    }
+
+    /**
+     * This method gets the binary location and sends to the getDriver to properly set the setProperty method.
+     * @param browser The browser type will either be chrome or firefox for now until we expand to browsers
+     * @return Returns the path for where the binaries are located in the untilites/browser folder.
+     */
+    private static String getBrowserPath(String browser) {
+        String currentOS = System.getProperty("os.name").toLowerCase();
+        String posixPath = "./src/main/java/utilities/browsers/";
+        String setPath = "";
+        /* TODO: This method needs ot be expand to edge once it has been added to the getDriver method */
+        if (currentOS.contains("mac")) {
+            switch (browser) {
+                case "chrome":
+                    setPath = posixPath + "chromedriver";
+                    break;
+                case "firefox":
+                    setPath = posixPath + "geckodriver";
+                    break;
+            }
+            /* TODO: Need to test this half of the method to see if it works properly on a windows machine */
+        } else if (currentOS.contains("windows")) {
+            switch (browser) {
+                case "chrome":
+                    setPath = posixPath + "chromedriver.exe";
+                    break;
+                case "firefox":
+                    setPath = posixPath + "geckodriver.exe";
+                    break;
+            }
+        } else {
+            System.out.println("unable to find OS Type");
+        }
+        return setPath;
     }
 }
